7675e090bf2c28155578fcb5d7722fc7e572bb9a
pedroquintao <pedro.h.quintao@hotmail.com>
2023-03-22 19:39:58 -0300

    inicio da criação da lista

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-//teste nova branch
+const list = document.querySelectorAll('.list__item__test');
+const form = document.querySelector('#form');
+
+itens = [];
+
+desenhaLista(); 
+
+function desenhaLista(){
+    list.forEach(elt => createItem(elt))
+}
+
+function createItem(item){
+    console.log(item)
+}
+
+form.addEventListener('submit', (evt) => {
+    
+    evt.preventDefault();
+    
+    const itemName = evt.target.elements['form-name'];
+    console.log(itemName.value)
+})
