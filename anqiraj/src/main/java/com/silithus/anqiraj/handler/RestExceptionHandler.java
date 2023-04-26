5596f807d62c3a087cba8a6fd0f3c3e85a30e80e
RiemannRoch <riemannroch@github.com>
2023-04-26 17:51:43 -0300

    Override Spring Handlers

--- anqiraj/src/main/java/com/silithus/anqiraj/handler/RestExceptionHandler.java
+++ anqiraj/src/main/java/com/silithus/anqiraj/handler/RestExceptionHandler.java
@@ -1,25 +1,107 @@
 package com.silithus.anqiraj.handler;
 
 import com.silithus.anqiraj.exception.BadRequestException;
-import com.silithus.anqiraj.exception.BadRequestExceptionDetails;
+import com.silithus.anqiraj.exception.ExceptionDetails;
+import com.silithus.anqiraj.exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+@Log4j2
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException exception) {
+    public ResponseEntity<ExceptionDetails> handleBadRequestException(BadRequestException exception) {
         return new ResponseEntity<>(
-                BadRequestExceptionDetails.builder()
+                ExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
                         .status(HttpStatus.BAD_REQUEST.value())
                         .details(exception.getMessage())
                         .developerMessage(exception.getClass().getName())
                         .title("Bad Request Exception")
                         .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request) {
+
+        String title;
+        if (ex.getMessage()
+                .contains("Cannot deserialize value of type `com.silithus.anqiraj.model.SlotEnum`")) {
+            title = "Invalid value for slot";
+        } else if (ex.getMessage()
+                .contains("Cannot deserialize value of type `com.silithus.anqiraj.model.RarityEnum`")) {
+            title = "Invalid value for rarity";
+        } else {
+            title = "Unexpected error";
+        }
+
+        return new ResponseEntity<>(
+                ExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .details(ex.getMessage())
+                        .title(title)
+                        .developerMessage(ex.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .details(ex.getMessage())
+                        .title("Bad Request Exception, Invalid Fields")
+                        .developerMessage(ex.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            @Nullable Object body,
+            @NonNull HttpHeaders headers,
+            HttpStatusCode statusCode,
+            @NonNull WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder().timestamp(LocalDateTime.now())
+                .status(statusCode.value())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .title(ex.getCause().getMessage())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, statusCode);
     }
 }
