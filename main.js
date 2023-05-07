ccf2e7d634cf12b2109f406abcabe6e83e1401f2
pedroquintao <pedro.h.quintao@hotmail.com>
2023-05-07 13:03:57 -0300

    Implementado os métodos GET e POST. O GET ainda precisa de ajustes

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const list = document.querySelector('#list-items'); // Seleciona o elemento com id "list-items" e o armazena na constante list
 const form = document.querySelector('#form'); //Seleciona o elemento com o id "form" e o armazena na constante form
+const url = 'http://localhost:8080/items';
 
-var items = JSON.parse(localStorage.getItem('forms-data')) || [];
-
-drawList(); 
+getItemsMethod(url);
 
 addSubimitListener(form);
 
 addClearLSButtonListener();
 
-function drawList(){
-    items.forEach(elt => createItem(elt));
+function drawList(data){
+    data.forEach(elt => createItem(elt));
+}
+
+function createItem(item) {
+
+    const newItem = document.createElement('div');
+
+    addInnerHtml(newItem, item);
+
+    addDeleteButtonListener(newItem, item);
+
+    addDropDownListener(newItem, item.Id);
+
+    drawItem(newItem);
+    
 }
 
 function addSubimitListener(forms) {
@@ -19,44 +32,30 @@ function addSubimitListener(forms) {
         
         evt.preventDefault();
         
-        const inputName = evt.target.elements['form-name'];
-        const inputSlot = evt.target.elements['form-slot'];
-        const inputRarity = evt.target.elements['form-rarity'];
-    
-        const inputItem = createInputItem(inputName, inputSlot, inputRarity);
-    
+        const inputFormsData = evt.target.elements;
+
+        const inputItem = createInputItem(inputFormsData);
+        
         uptadeOrCreateItem(inputItem);
+
+        localStorage.setItem("forms-data", JSON.stringify(items));        
         
-        localStorage.setItem("forms-data", JSON.stringify(items));
-    
-        // location.reload();
 
-        fetch('http://localhost:8080/items')
-            .then(response => response.json())
-            .then(data => console.table(data.content));
-
-        fetch('http://localhost:8080/items/6', {method: "DELETE"})
-
-        let body = {name: "ITEM 23", slot: "HEAD", rarity: "EPIC"}
-        
-        let headers = new Headers();
-        headers.set("Content-Type", "Application/json");
-
-        fetch('http://localhost:8080/items', {method: "POST", body: JSON.stringify(body), headers:headers})
-            .then(response => response.json())
-            .then(data => console.log(data));
-    
         clearForms();
     });
 }
 
-function createInputItem(inputName, inputSlot, inputRarity) {
 
-    return {
-        name: inputName.value,
-        slot: inputSlot.value,
-        rarity: inputRarity.value            
+
+function createInputItem(inputFormsData) {
+
+    const body = {
+        name: inputFormsData['form-name'].value,
+        slot: inputFormsData['form-slot'].value.toUpperCase(),
+        rarity: inputFormsData['form-rarity'].value.toUpperCase()            
         };
+
+    return body;
 };
 
 function uptadeOrCreateItem(inputItem) {
@@ -65,7 +64,7 @@ function uptadeOrCreateItem(inputItem) {
         
         const existentItem = checkItemExistence(inputItem.name); 
         inputItem.Id = existentItem.Id;
-        
+        putMethod();
         updateItem(inputItem);
     }
     
@@ -75,14 +74,15 @@ function uptadeOrCreateItem(inputItem) {
         inputItem.Id = itemId;
         
         createItem(inputItem);
-        
-        items.push(inputItem);
+        postItemMethod(inputItem, url);
         
     }
 }
 
 function checkItemExistence(name) {
+    
     return items.find(elt => elt.name === name);
+    
 };
 
 function checkLastItemId() {
@@ -97,25 +97,12 @@ function clearForms() {
     });
 }
 
-function createItem(item) {
-
-    const newItem = document.createElement('div');
-
-    addInnerHtml(newItem, item);
-
-    addDeleteButtonListener(newItem, item);
-
-    addDropDownListener(newItem, item.Id);
-
-    list.appendChild(newItem);
-}
-
 function addInnerHtml(element, item) {
 
     element.innerHTML = `<li class="list__items__item" data-id="${item.Id}" data-name="${item.name}" data-slot="${item.slot}" data-rarity="${item.rarity}">
                             <div class="list__items__item__box">
                                 <div class="list__items__item__box__itemname">
-                                    <strong class="list__items__item__box__itemname"__name>${item.name}</strong>
+                                    <strong class="list__items__item__box__itemname__name">${item.name}</strong>
                                 </div>
                                 <button class="item__delete__button">X</button>                 
                             </div>
@@ -138,11 +125,15 @@ function addDropDownListener(element, id) {
     const itemClickableArea = element.querySelector('.list__items__item__box__itemname')
 
     itemClickableArea.addEventListener('click', function() {
-        const listItem = document.querySelector(`[data-id="${id}"]`);
-        const dropDown = listItem.querySelector('.dropdown')
-        
+
+        const dropDown = element.querySelector('.dropdown')
         dropDown.classList.toggle('dropdown-show');
     })
+}
+
+function drawItem(item) {
+
+    list.appendChild(item);
 }
 
 function updateItem(item) {
@@ -169,22 +160,67 @@ function addClearLSButtonListener() {
     );
 }
 
-// function getTargetParentNode(initialChildNode, targetParentNode) {
+function getItemsMethod(url) {
 
-//     let currentParentNode = initialChildNode;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => drawList(data.content));
+}
 
-//     while(targetParentNode !== currentParentNode) {
-//         currentParentNode = currentParentNode.parentNode;
-//     };
+function postItemMethod(item, url) {
 
-//     return currentParentNode;
-// }
+    let headers = new Headers();
+    headers.set("Content-Type", "Application/json");
+
+    fetch(url, {method: "POST", body: JSON.stringify(item), headers:headers})
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+function putMethod() {
+
+}
+
+function deleteMethod() {
     
+}
+
+// fetch('http://localhost:8080/items/6', {method: "DELETE"})
+
+// let body = {name: "ITEM 23", slot: "HEAD", rarity: "EPIC"}
+
+// let headers = new Headers();
+// headers.set("Content-Type", "Application/json");
+
+// fetch('http://localhost:8080/items', {method: "POST", body: JSON.stringify(body), headers:headers})
+// .then(response => response.json())
+// .then(data => console.log(data));
 
 // =================================================================================================================
 //                                                     Next Step
 // =================================================================================================================
 
-//
+// - Adaptar o slot e o name do body na função postItemMethod() para maiúsculos. Deu conflito porque no codigo SlotEnum.java e RarityEnum.java por padrão do java, o Daniel colocou o enum com todas as palvras em caixa alta.
+// - O toUpperCase funcionou, mas ainda está acusando erro de formato de entrada
+// - Do jeito que está atualmente, o name também está usando o toUpperCase, lembrar de retirar o toUpperCase dele
+
+
+
+//Quando usar cada método REST:
+
+//GET : - Quando for pegar todos os itens da lista para apresentar na tela (provavelmente terá que armazenar o resultado no array items, semelhante como foi feito quando se utilizava o localStorage)
+// |
+//  `-> Provavelmente será utilizado na função drawList()
+    
+
+//POST: - Quando for enviar o formulário para criar um item novo
+
+//PUT: - Quando for atualizar um item (a atualização do item pode ser tanto por um botão para atualizar o item quanto quando há uma tentativa de adicionar um item com um nome de item que já existe no banco)
+
+//DELETE - Quando for apagar um item do banco
+
+
+
+
 
 
