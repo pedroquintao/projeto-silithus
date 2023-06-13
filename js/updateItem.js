faae63687cfea2ef397e0421360fc83beb4b07f6
pedroquintao <pedro.h.quintao@hotmail.com>
2023-06-13 20:27:47 -0300

    Problema resolvido
    -Para resolução do problema, foi repensada a forma que o modo de edição irá funcionar. Dessa forma, foi criada a função updateModeSetup que é utilizada para configurar se a aplicação está em modo de edição (updateMode) ou não. Além disso, todas as funções que alternavam características do DOM, quando iniciava-se o modo de edição, foram agregadas na função updateModeSetup.

--- js/updateItem.js
+++ js/updateItem.js
@@ -10,11 +10,9 @@ const submitButton = document.getElementById("submit-button");
 
 const updateEvent = (event) => {
     updateItem(event);
-    toggleSubmitButtonListener();
 }
 
-export let updateMode = false;
-let submitButtonMode = true;
+export let UPDATE_MODE = false;
 
 export function addUpdateListener(updateButton, item) {
     updateButton.addEventListener("click", updateButtonClick);
@@ -22,18 +20,13 @@ export function addUpdateListener(updateButton, item) {
 }
 
 const updateButtonClick = (event) => {
-    if(updateMode){
+    if(UPDATE_MODE){
         console.log('%cupdateItem.js line:51 Mensagem de erro: ', 'color: #007acc;', "Update mode is already on!");
         return;
     }
 
-    updateMode = true;
     const item = event.target.item;
-    fillForms(item);
-    //Talvez dê pra colocar todos esses toggles em uma função só
-    toggleSubmitButtonListener(item);
-    toggleHighLightMode(item);
-    toggleTitleWhenUpdateModeIsOn();
+    updateModeSetup(item, 'on');
 }
 
 function fillForms(data) {
@@ -41,39 +34,33 @@ function fillForms(data) {
     form.forEach((element, index) => element.value = normalizeWord(Object.values(data)[index + 1])); //Adicionar ao guia.js
 }
 
-function toggleSubmitButtonListener(item) {
-    
-    const dataSubmit = document.querySelector("[data-submit]"); 
-
-    if(submitButtonMode === true) {
-        dataSubmit.removeEventListener("submit", submitEvent);
-        dataSubmit.addEventListener("submit", updateEvent)
-        submitButtonMode = false;
-        submitButton.innerHTML = "<b>Update</b>"
-    }
-
-    else if(submitButtonMode === false) {
-        dataSubmit.removeEventListener("submit", updateEvent);
-        dataSubmit.addEventListener("submit", submitEvent);
-        submitButtonMode = true;
-        submitButton.innerHTML = "<b>Register</b>";
-
-    }
-    dataSubmit.itemData = item;
-}
-
-
-
-function toggleHighLightMode(item) {
+function updateModeSetup(item, state) {
+    const submitData = document.querySelector("[data-submit]"); 
+    submitData.itemData = item;
     const itemBar = document.querySelector(`[data-id="${item.id}"]`)
     const formBars = document.querySelectorAll(".form__bar");
     const highlightElements = [itemBar];
     formBars.forEach(element => highlightElements.push(element));
-    highlightElements.forEach(element => element.classList.toggle("highlight"));
-}
-function toggleTitleWhenUpdateModeIsOn() {
-    document.querySelector("[data-form-title]").innerHTML = `<h1 class="title1" id="item-registration">Update Mode</h1>
-    <p class="paragraph1">Fill in the forms with the data of the item you want to update:</p>`;
+
+    switch(state) {
+        case 'on':
+            fillForms(item);
+            submitData.removeEventListener("submit", submitEvent);
+            submitData.addEventListener("submit", updateEvent);
+            submitButton.innerHTML = "<b>Update</b>";
+            document.querySelector("[data-form-title]").innerHTML = '<h1 class="title1" id="item-registration">Update Mode</h1><p class="paragraph1">Fill in the forms with the data of the item you want to update:</p>';
+            highlightElements.forEach(element => element.classList.toggle("highlight"));
+            console.log('%cupdateItem.js line:60 object', 'color: #007acc;', "Rodo");
+            return UPDATE_MODE = true;
+        
+        case'off':
+            submitData.removeEventListener("submit", updateEvent);
+            submitData.addEventListener("submit", submitEvent);
+            submitButton.innerHTML = "<b>Register</b>";
+            document.querySelector("[data-form-title]").innerHTML = '<h1 class="title1" id="item-registration">Item Registration</h1><p class="paragraph1">Fill in the forms with the data of the item you want to register:</p>';
+            highlightElements.forEach(element => element.classList.toggle("highlight"));
+            return UPDATE_MODE = false;
+    }
 }
 
 async function updateItem(event) {
@@ -90,5 +77,5 @@ async function updateItem(event) {
     await apiRequests.putItem(url, currentFormInputData);    
     showItems.buildList();
     clearForms();
-    updateMode = false;
+    updateModeSetup(currentFormInputData, 'off');
 }
